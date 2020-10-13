4915cbe7e73b4ae6aba3bfe0fa7c0b2be9646fb5
Patrick Grahm <patrick.grahm@gmx.de>
2020-10-13 17:04:05 +0200

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,29 +1,10 @@
-﻿function Save-GitHubRepository
-{
-    param(
-        [Parameter(Mandatory)]
-        [string]$Owner,
-
-        [Parameter(Mandatory)]
-        [string]$Repository,
-
-        [Parameter(Mandatory)]
-        [string]$Branch,
-
-        [Parameter()]
-        [string]$DestinationPath
-    )
-
-    $url = "https://github.com/$Owner/$Project/archive/$Branch.zip"
-    $output = Join-Path $HOME "Desktop\${Project}-${Branch}_($(Get-Date -Format yyyyMMddHHmm)).zip"
-    $start_time = Get-Date
-
-    $wc = New-Object System.Net.WebClient
-    $wc.DownloadFile($url, $output)
-
-    Write-Host "Time taken: $((Get-Date).Subtract($start_time).TotalSeconds) second(s)" 
-}
-
-$Downloadpfad = (Get-Location)
-
-Save-GitHubRepository xpati99x PowerShell master
+﻿$Url = 'https://github.com/xpati99x/Effzett-Installation/archive/master.zip' 
+$ZipFile = $(Split-Path -Path $Url -Leaf) 
+$Destination= 'C:\EffzettInstall\' 
+ 
+Invoke-WebRequest -Uri $Url -OutFile $ZipFile 
+ 
+$ExtractShell = New-Object -ComObject Shell.Application 
+$Files = $ExtractShell.Namespace($ZipFile).Items() 
+$ExtractShell.NameSpace($Destination).CopyHere($Files) 
+Start-Process $Destination

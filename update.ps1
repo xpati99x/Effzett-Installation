5764295e33cafe148c209fa293112bca5673075b
Patrick Grahm <patrick.grahm@gmx.de>
2020-10-13 16:09:09 +0200

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,40 +1,29 @@
-﻿function DownloadFilesFromRepo {
-Param(
-    [string]$Owner ="xpati99x",
-    [string]$Repository ="PowerShell",
-    [string]$Path, 
-    [string]$DestinationPath = (Get-Location)
+﻿function Save-GitHubRepository
+{
+    param(
+        [Parameter(Mandatory)]
+        [string]$Owner,
+
+        [Parameter(Mandatory)]
+        [string]$Repository,
+
+        [Parameter(Mandatory)]
+        [string]$Branch,
+
+        [Parameter()]
+        [string]$DestinationPath
     )
 
-    $baseUri = "https://api.github.com/"
-    $args = "repos/$Owner/$Repository/contents/$Path"
-    $wr = Invoke-WebRequest -Uri $($baseuri+$args)
-    $objects = $wr.Content | ConvertFrom-Json
-    $files = $objects | where {$_.type -eq "file"} | Select -exp download_url
-    $directories = $objects | where {$_.type -eq "dir"}
-    
-    $directories | ForEach-Object { 
-        DownloadFilesFromRepo -Owner $Owner -Repository $Repository -Path $_.path -DestinationPath $($DestinationPath+$_.name)
-    }
+    $url = "https://github.com/$Owner/$Project/archive/$Branch.zip"
+    $output = Join-Path $HOME "Desktop\${Project}-${Branch}_($(Get-Date -Format yyyyMMddHHmm)).zip"
+    $start_time = Get-Date
 
-    
-    if (-not (Test-Path $DestinationPath)) {
-        # Destination path does not exist, let's create it
-        try {
-            New-Item -Path $DestinationPath -ItemType Directory -ErrorAction Stop
-        } catch {
-            throw "Could not create path '$DestinationPath'!"
-        }
-    }
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile($url, $output)
 
-    foreach ($file in $files) {
-        $fileDestination = Join-Path $DestinationPath (Split-Path $file -Leaf)
-        try {
-            Invoke-WebRequest -Uri $file -OutFile $fileDestination -ErrorAction Stop -Verbose
-            "Grabbed '$($file)' to '$fileDestination'"
-        } catch {
-            throw "Unable to download '$($file.path)'"
-        }
-    }
-
+    Write-Host "Time taken: $((Get-Date).Subtract($start_time).TotalSeconds) second(s)" 
 }
+
+$Downloadpfad = (Get-Location)
+
+Save-GitHubRepository xpati99x PowerShell master

95ecbb0758d73596ce36081b3c06867bc50cf669
Patrick Grahm <patrick.grahm@gmx.de>
2020-10-13 16:41:32 +0200

    Update RunFromUSB.bat

--- RunFromUSB.bat
+++ RunFromUSB.bat
@@ -3,7 +3,6 @@ mkdir c:\EffzettInstall
 xcopy "%~dp0*.*" "C:\EffzettInstall\*\*.*" /E /H /C /R /Q /Y
 REM Enable execution policy to allow running of scripts
 Powershell.exe -Command "& {Start-Process Powershell.exe -ArgumentList 'set-executionpolicy remotesigned' -Verb RunAs}"
-REM The download.ps1 auto extracts the contents to c:\build. Wait 10 seconds for this to complete before executing the PCSetup.ps1 script
 timeout 10
 REM Execute the final build script
 powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"c:\EffzettInstall\EffzettInstall.ps1\"' -verb RunAs}"
